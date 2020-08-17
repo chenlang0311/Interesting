e2d7603fe848de897fb5bb0c605c12dcce9f756b
chenlang <q1025518502@qq.com>
2020-08-17 18:50:50 +0800

    feat:最近几个月的时间获取

--- src/pc.js
+++ src/pc.js
@@ -1,15 +1,10 @@
 let today = new Date().toLocaleDateString();
 let todayStart = new Date(today).getTime()
 let todayEnd = new Date(today + ' 23:59:59').getTime()
-console.log(today,todayStart, todayEnd)
 let year = new Date().getFullYear();
-console.log(year)
 let month = new Date().getMonth() + 1
-console.log(month)
 let day = new Date().getDate();
-console.log(day)
 let nearSevenDay = [todayStart - 86400000 * 7,todayEnd]
-console.log(nearSevenDay)
 let nearMonthStart = ''
 if(month > 1){
   nearMonthStart = new Date(year+'/'+(month-1) + '/' + day).getTime()
@@ -17,7 +12,6 @@ if(month > 1){
   nearMonthStart = new Date((year - 1)+'/'+ 12 + '/' + day).getTime()
 }
 let nearMonth = [nearMonthStart, todayEnd]
-console.log(nearMonth)
 let nearThreeMonthStart = ''
 if(month>3){
   nearThreeMonthStart = new Date(year+'/'+(month-3) + '/' + day).getTime()
@@ -25,7 +19,6 @@ if(month>3){
   nearThreeMonthStart = new Date((year - 1)+'/'+ (9  + month) + '/' + day).getTime()
 }
 let nearThreeMonth = [nearThreeMonthStart, todayEnd]
-console.log(nearThreeMonth)
 let nearHalfYearStart = '';
 if(month>6){
   nearHalfYearStart = new Date(year+'/'+(month-6) + '/' + day).getTime()
@@ -33,7 +26,25 @@ if(month>6){
   nearHalfYearStart = new Date((year - 1)+'/'+ (6  + month) + '/' + day).getTime()
 }
 let nearHalfYear = [nearHalfYearStart, todayEnd]
-console.log(nearHalfYear)
 let nearYearStart = new Date((year - 1)+'/'+ month + '/' + day).getTime()
 let nearYear = [nearYearStart, todayEnd]
-console.log(nearYear)
+
+
+function getNearMonthStart(months){
+  let lastsYear = Math.ceil(months / 12);
+  let month = months % 12;
+  let currntYear = new Date().getFullYear();
+  let currntMonth = new Date().getMonth() + 1;
+  let currntDay = new Date().getDate();
+  console.log('l,m',lastsYear,month,months)
+  if(currntMonth > month){
+    return new Date((currntYear - (months >=12 ? lastsYear: 0))+'/'+(currntMonth-month) + '/' + currntDay).getTime()
+  }else{
+    return new Date((currntYear - lastsYear)+'/'+ ((12- month )  + currntMonth) + '/' + currntDay).getTime()
+  }
+}
+let sss = getNearMonthStart(24)
+console.log('sss',sss)
+
+nearThreeMonth = [getNearMonthStart(3),todayEnd]
+console.log(nearThreeMonth)
